3a059312c7f8ed52bef015334a7bc9362dac6f6b
Denis Zangarov <denisimo_98@yahoo.com>
2020-06-21 22:20:30 +0300

    Update Vector.h

--- Vectors/Vectors/Vector.h
+++ Vectors/Vectors/Vector.h
@@ -13,30 +13,30 @@ private:
 	float vz;
 
 public:
-	virtual ~Vector();						//Destructor
-	Vector();								//Default Constructor
-	Vector(float, float, float);		    //Constructor with Parameters
-	Vector(const Point&, const Point&);		//Vector set with two Points in 3D space
-	Vector(const Vector&);					//Copy Constructor
-	Vector& operator=(const Vector&);		//predefinition of equalization
+	virtual ~Vector();			//Destructor
+	Vector();				//Default Constructor
+	Vector(float, float, float);		//Constructor with Parameters
+	Vector(const Point&, const Point&);	//Vector set with two Points in 3D space
+	Vector(const Vector&);			//Copy Constructor
+	Vector& operator=(const Vector&);	//predefinition of equalization
 
-	bool isItZero() const;					 //check if it is the Zero Vector
-	bool Parallel(const Vector&) const;		 //check if two Vectors are Parallel
+	bool isItZero() const;			 //check if it is the Zero Vector
+	bool Parallel(const Vector&) const;	 //check if two Vectors are Parallel
 	bool Perpendicular(const Vector&) const; //check if two Vectors are Perpendicular
 	Vector Projection(const Vector&) const;  //finds projection of one Vector onto another
 
 
-	double Length() const;								    //finds the Length of a Vector
-	Vector Direction() const;							    //finds the Direction of the Vector
+	double Length() const;		         //finds the Length of a Vector
+	Vector Direction() const;	         //finds the Direction of the Vector
 
 
-	Vector operator+(const Vector&) const;				    //Addition of two Vectors
-	Vector operator-(const Vector&) const;				    //Substraction of two Vectors
-	Vector operator*(const double) const;				    //Multiplication of a Vector with a number
-	double operator*(const Vector&) const;				    //Scalar Multiplication of two Vectors
-	Vector operator^(const Vector&) const;					//Cross Product
+	Vector operator+(const Vector&) const;			//Addition of two Vectors
+	Vector operator-(const Vector&) const;			//Substraction of two Vectors
+	Vector operator*(const double) const;			//Multiplication of a Vector with a number
+	double operator*(const Vector&) const;			//Scalar Multiplication of two Vectors
+	Vector operator^(const Vector&) const;			//Cross Product
 	double operator()(const Vector&, const Vector&) const;  //Combined Multiplication
-	bool operator==(const Vector&) const;				    //Checks if two Vectors are the same
+	bool operator==(const Vector&) const;			 //Checks if two Vectors are the same
 
 	int setVectorX(float);
 	int setVectorY(float);
@@ -49,8 +49,8 @@ public:
 	int setVector(Vector);
 	Vector getVector() const; //return the whole Vector
 
-	virtual void print(std::ofstream&) const;									  //output when manual inputting
-	virtual void autoPrint(std::ofstream& out, int* arr, unsigned short b) const; //output when reading from a file
+	virtual void print(std::ofstream&) const;					//output when manual inputting
+	virtual void autoPrint(std::ofstream& out, int* arr, unsigned short b) const;   //output when reading from a file
 
 	virtual std::ostream& ext(std::ostream& out) const;
 	virtual std::istream& ins(std::istream& in, bool = 0, int* = nullptr, unsigned short = 0);
